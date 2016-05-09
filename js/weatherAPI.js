0a078e3ec069cd57c4f3a3597d8bc6f65b17b8b9
OSP123 <omarpatel123@gmail.com>
2016-05-08 17:13:15 -0700

    updated api

--- js/weatherAPI.js
+++ js/weatherAPI.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
   var lat, lon;
   var appID = "a4b0ab90a28dc5c1f397f762eb6efe76";
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
+
+ function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+      }
+  }
+
+  function showPosition(position) {
       lat = position.coords.latitude;
-      lon = position.coords.longitude;
-    })
+      lon = position.coords.longitude; 
   }
 
   var queryURL = "api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&APPID=" + appID;
