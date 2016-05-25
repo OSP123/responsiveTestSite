c41e3e075cb92ce8a8d0bb4d90ef4b63ec533232
OSP123 <omarpatel123@gmail.com>
2016-05-24 18:41:14 -0700

    Added alot of images and improved look

--- js/script.js
+++ js/script.js
@@ -1,14 +1,7 @@
 $( document ).ready(function() {
-	$(".changeAdd").click(function () {
-		$("h1").addClass("potato");
-	});
-	$(".changeRemove").click(function () {
-		$("h1").removeClass("potato");
-	});
-	$(".theme1").click(function () {
-		$("h1").toggleClass("h1_pretty");
-		$("body").toggleClass("body_pretty");
-		$("div").toggleClass("div_pretty");
-	});
-	$(".cool").attr("href", "https://www.google.com");
+    $(".home_race_image_link").hover(function(){
+        $(this).stop().animate({'bottom': '8px'}, 500);
+    }, function(){
+        $(this).stop().animate({'bottom': '0px'}, 500);
+    });
 });
